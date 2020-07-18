f451f39e0c8c7f96d533eaa4846b9566c87891d6
Kostas Makedos <kostas.makedos@gmail.com>
2020-07-18 22:51:20 +0300

    Add First Empty Class

---
+++ Numbers.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Kostas Makedos on 18/07/2020.
+// Email: kostas.makedos@gmail.com
+// Copyright (c) 2020 All rights reserved.
+//
+
+#include "Numbers.h"
---
+++ Numbers.h
@@ -0,0 +1,16 @@
+//
+// Created by Kostas Makedos on 18/07/2020.
+// Email: kostas.makedos@gmail.com
+// Copyright (c) 2020 All rights reserved.
+//
+
+#ifndef CPP_COOKBOOK_NUMBERS_H
+#define CPP_COOKBOOK_NUMBERS_H
+
+
+class Numbers {
+
+};
+
+
+#endif //CPP_COOKBOOK_NUMBERS_H
