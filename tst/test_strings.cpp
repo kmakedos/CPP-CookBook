d84f29726b3fb7b69e71b28a62fc5644543a5588
Kostas Makedos <kostas.makedos@gmail.com>
2020-08-02 17:33:47 +0300

    Add pad and trim functions

--- tst/test_strings.cpp
+++ tst/test_strings.cpp
@@ -29,7 +29,24 @@ BOOST_AUTO_TEST_SUITE(testStrings)
         std::wstring trim02= L"Κωστας Μακέδοςςςς.ςςς";
         trimString(trim02, L'ς');
         BOOST_CHECK(trim02 == L"Κωστας Μακέδοςςςς.");
+    }
+    BOOST_AUTO_TEST_CASE(TEST_STRINGSEQUENCE) {
+        std::vector<std::string> str_vec;
+        for (int i = 0 ;i < 100; i++){
+            str_vec.push_back("TestString");
+        }
+        std::cout << "Vector size :"  << str_vec.size() << std::endl;
+    }
 
+    BOOST_AUTO_TEST_CASE(TEST_STRINGLENGTH) {
+        std::string tst = "Solop";
+        std::string::size_type init_len = tst.length();
+        std::string::size_type init_cap = tst.capacity();
+        tst += "123456789012345678";
+        std::string::size_type final_len = tst.length();
+        std::string::size_type final_cap = tst.capacity();
+        BOOST_CHECK_GE(final_len, init_len);
+        BOOST_CHECK_GE(final_cap, init_cap);
     }
 
 BOOST_AUTO_TEST_SUITE_END()
