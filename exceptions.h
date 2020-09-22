10f2cdba12faeb30e81785ba1a2f81fa2768aa18
Kostas Makedos <kostas.makedos@gmail.com>
2020-09-23 01:00:30 +0300

    Add empty exceptions files

---
+++ exceptions.h
@@ -0,0 +1,8 @@
+//
+// Created by kostas on 23/9/20.
+//
+
+#ifndef CPP_COOKBOOK_EXCEPTIONS_H
+#define CPP_COOKBOOK_EXCEPTIONS_H
+
+#endif //CPP_COOKBOOK_EXCEPTIONS_H
---
+++ tst/test_exceptions.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Kostas Makedos on 18/07/2020.
+// Email: kostas.makedos@gmail.com
+// Copyright (c) 2020 All rights reserved.
+//
+
+#define BOOST_TEST_DYN_LINK
+#include <list>
+#include <boost/test/unit_test.hpp>
+#include <vector>
+#include <algorithm>
+#include <exception>
+#include "../classes.h"
+
+BOOST_AUTO_TEST_SUITE(testExceptions)
+
+    BOOST_AUTO_TEST_CASE(TEST_EXCEPTION_CLASS){
+    }
+BOOST_AUTO_TEST_SUITE_END()
