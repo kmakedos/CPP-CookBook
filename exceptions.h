62fa7e81c1f7d37f52cf47432354c6eb567f7133
Kostas Makedos <kostas.makedos@gmail.com>
2020-09-25 21:53:15 +0300

    Add latest exceptions files

--- exceptions.h
+++ exceptions.h
@@ -4,5 +4,49 @@
 
 #ifndef CPP_COOKBOOK_EXCEPTIONS_H
 #define CPP_COOKBOOK_EXCEPTIONS_H
+#include <iostream>
+#include <utility>
+#include <stdexcept>
+
+class MyException{
+public:
+    explicit MyException(std::string msg):Message(std::move(msg)){}
+    ~MyException()= default;
+    std::string getMessage()const {
+        return Message;
+    }
+private:
+    std::string Message;
+};
+
+class SafeDevice{
+public:
+    SafeDevice(int no){
+        if (no==2){
+            throw std::runtime_error("Problem in 2");
+        }
+    }
+    ~SafeDevice(){}
+};
+
+class Machine{
+
+public:
+    Machine(int devno1, int devno2):dev1(NULL), dev2(NULL){
+        try{
+            dev1 = new SafeDevice(devno1);
+            dev2 = new SafeDevice(devno2);
+        }
+        catch (std::runtime_error &e) {
+            std::cerr << "Error: " << e.what() << std::endl;
+
+        }
+
+    }
+
+private:
+    SafeDevice* dev1;
+    SafeDevice* dev2;
+};
 
 #endif //CPP_COOKBOOK_EXCEPTIONS_H
--- tst/test_exceptions.cpp
+++ tst/test_exceptions.cpp
@@ -10,10 +10,26 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
-#include "../classes.h"
+#include "../exceptions.h"
+void f(){
+    throw(MyException("Hello world except you!"));
+}
 
 BOOST_AUTO_TEST_SUITE(testExceptions)
 
-    BOOST_AUTO_TEST_CASE(TEST_EXCEPTION_CLASS){
+   BOOST_AUTO_TEST_CASE(TEST_EXCEPTION_CLASS){
+       try {
+                f();
+       }
+       catch (MyException &e) {
+           std::cout << e.getMessage() << std::endl;
+       }
+
+   }
+    BOOST_AUTO_TEST_CASE(TEST_SAFE_CLASS){
+
+        BOOST_CHECK_NO_THROW(Machine car = Machine(1,2));
+
     }
+
 BOOST_AUTO_TEST_SUITE_END()
