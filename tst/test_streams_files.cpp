20ed7b70328beed1ff1c7709bb6466fcc86fdeea
Kostas Makedos <kostas.makedos@gmail.com>
2020-09-29 01:13:18 +0300

    Adding directory iteration also

--- tst/test_streams_files.cpp
+++ tst/test_streams_files.cpp
@@ -9,6 +9,7 @@
 #include "../streams_files.h"
 #include <iomanip>
 #include <iostream>
+#include <cstdio>
 #include <fstream>
 #include <ctime>
 #include <sys/types.h>
@@ -57,6 +58,9 @@ BOOST_AUTO_TEST_SUITE(testStreamsFiles)
        std::cout << "Device: " << fileInfo.st_dev << std::endl;
        std::cout << "Created: " << std::ctime(&fileInfo.st_ctime) << std::endl;
        std::cout << "Modified: " << std::ctime(&fileInfo.st_mtime) << std::endl;
+       if (remove("test_file02_copy.txt") == -1){
+           std::cerr << "Error deleting file" << std::endl;
+       }
     }
     BOOST_AUTO_TEST_CASE(TEST_FILE_COPY) {
        std::ifstream in("test_file02.txt", std::ios_base::in | std::ios_base::binary);
@@ -72,10 +76,27 @@ BOOST_AUTO_TEST_SUITE(testStreamsFiles)
 
        std::string in_file = "test_file03.txt";
        std::string out_file = "test_file03_copy.txt";
+       if (boost::filesystem::remove(out_file) == -1){
+           std::cout << "We could not delete " << out_file << std::endl;
+       }
        boost::filesystem::path src = boost::filesystem::complete(boost::filesystem::path(in_file));
        boost::filesystem::path dst = boost::filesystem::complete(boost::filesystem::path(out_file));
        boost::filesystem::copy(src, dst);
 
     }
+    BOOST_AUTO_TEST_CASE(TEST_DIRECTORY_LISTING) {
+        boost::filesystem::path full_path = boost::filesystem::system_complete("/");
+        if (!boost::filesystem::exists(full_path)){
+            std::cerr << "We are in trouble" << std::endl;
+        }
+        if (boost::filesystem::is_directory(full_path)){
+            std::cout << "It is a folder though" << std::endl;
+        }
+
+        boost::filesystem::directory_iterator end;
+        for (boost::filesystem::directory_iterator it(full_path); it != end; ++it){
+            std::cout << it->path() << std::endl;
+        }
+}
 
 BOOST_AUTO_TEST_SUITE_END()
